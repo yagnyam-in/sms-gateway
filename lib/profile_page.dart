5ee44529f63af494a62a2d552afa9b8c74674605
Chenna Reddy <chenna@alavala.in>
2019-06-14 15:48:19 +0200

    showing own phone number on profile page

--- lib/profile_page.dart
+++ lib/profile_page.dart
@@ -31,7 +31,6 @@ class ProfilePage extends StatefulWidget {
 
 class _ProfilePageState extends State<ProfilePage> with GenericHelper {
   final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
-  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
   final AppState appState;
   final AuthChangeCallback authChangeCallback;
 
@@ -54,23 +53,26 @@ class _ProfilePageState extends State<ProfilePage> with GenericHelper {
       ),
       body: Padding(
         padding: EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 8.0),
-        child: Form(
-          key: _formKey,
-          child: ListView(
-            children: <Widget>[
-              const SizedBox(height: 8.0),
-              Icon(Icons.account_circle, size: 64.0),
-              const SizedBox(height: 16.0),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 32),
-                child: RaisedButton.icon(
-                  onPressed: _deleteAccount,
-                  icon: Icon(Icons.delete),
-                  label: Text("Delete Account"),
-                ),
+        child: ListView(
+          children: <Widget>[
+            const SizedBox(height: 8.0),
+            Icon(Icons.account_circle, size: 64.0),
+            const SizedBox(height: 24.0),
+            Center(
+              child: Text(
+                appState.firebaseUser.phoneNumber,
               ),
-            ],
-          ),
+            ),
+            const SizedBox(height: 16.0),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 32),
+              child: RaisedButton.icon(
+                onPressed: _deleteAccount,
+                icon: Icon(Icons.delete),
+                label: Text("Delete Account"),
+              ),
+            ),
+          ],
         ),
       ),
     );
